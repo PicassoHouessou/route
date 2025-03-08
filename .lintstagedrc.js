a98f3aa8f1e29a7daebe337bd8bac2d10ae6674c
Picasso Houessou <houessoupicasso@yahoo.fr>
2025-03-08 01:10:27 +0100

    Improve lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const prettierCommand = (filenames) =>
    `prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --loglevel warn --write`;
+const addProcessedFilesByPrettierCommand = (filenames) =>
+   `prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --loglevel warn --write`;
+
+console.log(prettierCommand);
 
 module.exports = {
    '*.{js,ts}': [
       //"ng lint",
       prettierCommand,
+      addProcessedFilesByPrettierCommand,
    ],
 };
